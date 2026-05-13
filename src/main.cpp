c3d1f78f9128b1c0cc5a1ca3e7de312816bc9658
Karson Du <karsondu@berkeley.edu>
2026-05-12 18:09:09 -0700

    Changed  Main.cpp from hard coded n and m to command line inputs for n and m and seed

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,31 @@
 #include "Simulation.h"
 #include <iostream>
 
+
 int main() {
-    int numTrucks = 10;
-    int numStations = 2;
-    /*std::cout << "Enter Number of Trucks(n): ";
+    int numTrucks;
+    int numStations;
+    unsigned int seed;
+    std::string seedInput;
+
+    std::cout << "Enter Number of Trucks(n): ";
     std::cin>>numTrucks;
     std::cout << "Enter Number of Stations(m): ";
-    std::cin>>numStations;*/
+    std::cin>>numStations;
 
-    Simulation sim(numTrucks, numStations);
+    std::cin.ignore(); // clear buffer
+    std::cout << "Enter seed or press Enter for random: ";
+    std::getline(std::cin, seedInput);
+
+    if (seedInput.empty()) {
+        seed = time(0);
+        std::cout << "Using random seed: " << seed << "\n";
+    } else {
+        seed = std::stoi(seedInput);
+    }
+
+
+    Simulation sim(numTrucks, numStations,seed);
     sim.run();
     sim.printStatistics();
 
