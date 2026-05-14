caa0c471c80a2b7d7fc308030c20952730518f2e
Karson Du <karsondu@berkeley.edu>
2026-05-14 00:22:11 -0700

    Initial Documentation for main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,13 @@
 
 
 int main() {
+    //Instantiate variables for numTrucks(n), numStations(m), the seed for randomness, and seedInput for user input to the seed to check if we are using a user given seed or a random seed
     int numTrucks;
     int numStations;
     unsigned int seed;
     std::string seedInput;
 
+    //Prompt the user for n and m and verify that they are both valid inputs greater than 0
     std::cout << "Enter Number of Trucks(n): ";
     std::cin>>numTrucks;
     std::cout << "Enter Number of Stations(m): ";
@@ -17,10 +19,12 @@ int main() {
         return 0;
     }
 
-    std::cin.ignore(); // clear buffer
+    //Clear up the input buffer since previous inputs leave \n, and prompt the user for a seed and put the user's input into seedInput
+    std::cin.ignore(); 
     std::cout << "Enter seed or press Enter for random: ";
     std::getline(std::cin, seedInput);
 
+    //Check to see if a seed was given, if not then we use a random seed based on time, if so we just convert the user's input into an int that gets used as the seed
     if (seedInput.empty()) {
         seed = time(0);
         std::cout << "Using random seed: " << seed << "\n";
@@ -28,7 +32,7 @@ int main() {
         seed = std::stoi(seedInput);
     }
 
-
+    //We create a Simulation object using numTrucks, numStations, and seed, then we run the sim, and afterwards can print out the statistics from it
     Simulation sim(numTrucks, numStations,seed);
     sim.run();
     sim.printStatistics();
